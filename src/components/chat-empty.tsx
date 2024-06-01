1876698d029a6c69b6f63b7029c356ea3c228310
Bobby Lee <superbahbi@gmail.com>
2024-05-31 19:39:10 -0700

    Create chat-empty.tsx

---
+++ src/components/chat-empty.tsx
@@ -0,0 +1,8 @@
+export function ChatEmpty() {
+  return (
+    <div className="w-full mt-24 flex flex-col items-center justify-center text-center">
+      {/* <Icons.LogoSmall /> */}
+      <span className="font-medium text-xl mt-6">No messages yet</span>
+    </div>
+  );
+}
